fe31e436816ec093a6086c52ef3fca839d9e0aa4
themmj <themmj@users.noreply.github.com>
2021-03-06 21:25:26 +0100

    Add connection establishing event handler

--- src/handler.ts
+++ src/handler.ts
@@ -1,20 +1,78 @@
 import log from './log';
 import * as sio from 'socket.io';
+import { v4 as uuidv4 } from 'uuid';
 
-// handler
-function onCreate(message: string, additionalMessage: string): void {
-    log.info("received message: %s with addition: %s", message, additionalMessage);
-}
+class Game {
+    readonly id: string;
+    private _offer: string;
+    private _hasValidOffer: boolean;
+    private _adminSocket: sio.Socket;
 
-// mapping assignment
-const handlerMap = new Map<string, (...args: any[]) => void>();
+    constructor(adminSocket: sio.Socket, initialOffer: string) {
+        this.id = uuidv4();
+        this._adminSocket = adminSocket;
+        this._offer = initialOffer;
+        this._hasValidOffer = true;
+        log.debug("created game %s with offer %s", this.id, initialOffer);
+    }
 
-handlerMap.set("create", onCreate);
+    set offer(o: string) {
+        log.debug("waiting to set new valid offer");
+        while (this._hasValidOffer);
+        log.debug("setting new valid offer");
+        this._offer = o;
+        this._hasValidOffer = true;
+    }
+    get offer(): string {
+        log.debug("waiting to get new valid offer");
+        while (!this._hasValidOffer);
+        log.debug("getting new valid offer");
+        this._hasValidOffer = false;
+        return this._offer;
+    }
 
-// export
+    sendToAdmin(eventName: string, ...args: any[]): void {
+        this._adminSocket.emit(eventName, args);
+    }
+};
+
+const gameMap = new Map<string, Game>();
+
+
 export default function addEventHandler(socket: sio.Socket): void {
-    handlerMap.forEach((handler: (...args: any[]) => void, eventName: string) => {
-        socket.on(eventName, handler);
+    socket.on("create", (offer: string) => {
+        const game = new Game(socket, offer);
+        gameMap.set(game.id, game);
+        game.sendToAdmin("gameCreated", game.id);
     });
+
+    socket.on("offer", (gameId: string, newOffer: string) => {
+        const game = gameMap.get(gameId);
+        if (game === undefined) {
+            log.warn("tried to set new offer on non existent game");
+            return;
+        }
+        game.offer = newOffer;
+    });
+
+
+    socket.on("join", (gameId: string) => {
+        const game = gameMap.get(gameId);
+        if (game === undefined) {
+            log.warn("tried to join non existent game");
+            return;
+        }
+        socket.emit("offer", game.offer);
+    });
+
+    socket.on("answer", (gameId: string, name: string, answer: string) => {
+        const game = gameMap.get(gameId);
+        if (game === undefined) {
+            log.warn("tried to answer to non existent game");
+            return;
+        }
+        game.sendToAdmin("answer", name, answer);
+    });
+
 }
 
